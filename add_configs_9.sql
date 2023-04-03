75d1d6a3dcffb0091cb1ba35efc8878de4605567
mwilinskixtm <42145313+mwilinskixtm@users.noreply.github.com>
2023-04-03 11:37:15 +0200

    Add files via upload
    TEST 2

---
+++ add_configs_9.sql
@@ -0,0 +1,5 @@
+INSERT INTO configuration (id, eloqua_user_id, key, value) VALUES (5,9,'xtmApiCompanyName','XTMDemo');
+INSERT INTO configuration (id, eloqua_user_id, key, value) VALUES (2,9,'xtmApiCustomerId','1181334');
+INSERT INTO configuration (id, eloqua_user_id, key, value) VALUES (3,9,'xtmApiTemplateId','1181340');
+INSERT INTO configuration (id, eloqua_user_id, key, value) VALUES (1,9,'xtmApiUrl','https://tcl1.xtm.cloud/');
+INSERT INTO configuration (id, eloqua_user_id, key, value) VALUES (4,9,'xtmApiWorkflowId','3081');
